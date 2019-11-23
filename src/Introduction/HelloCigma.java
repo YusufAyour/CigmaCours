658bba16996e8711a94281d40a840a0c7d191d67
Imzoughene Youssef <benfarice@gmail.com>
2019-11-23 14:43:16 +0100

    initial commit

---
+++ src/Introduction/HelloCigma.java
@@ -0,0 +1,11 @@
+package Introduction;
+
+public class HelloCigma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The cat looks like a Dog");
+
+	}
+
+}
