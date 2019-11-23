eb87a06f367311dda8a47c7618385ed6f99cc5b0
Imzoughene Youssef <benfarice@gmail.com>
2019-11-23 16:03:49 +0100

    equals

--- src/seance5_1/Shoes.java
+++ src/seance5_1/Shoes.java
@@ -8,4 +8,11 @@ public class Shoes extends Mother {
 		// TODO Auto-generated method stub
 		return super.toString();
 	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		Shoes m = (Shoes)obj;
+//		return super.equals(obj);
+		return this.Url.equals(m.Url);
+	}
 }
