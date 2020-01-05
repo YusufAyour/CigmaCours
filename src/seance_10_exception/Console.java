dab0441a52d6eb799a43bb4643431675c5998836
Imzoughene Youssef <benfarice@gmail.com>
2020-01-05 10:44:06 +0100

    seance 10 File Output Stream IO Exception

--- src/seance_10_exception/Console.java
+++ src/seance_10_exception/Console.java
@@ -5,20 +5,37 @@ import java.io.IOException;
 
 public class Console {
 
+	public static FileOutputStream f;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 2;
+		try {
+			div(a,b);
+		}catch(IOException e) {
+			System.out.println(e.getStackTrace());
+		}catch(Exception e) {
+			System.out.println(e.getStackTrace());
+		}finally {
+//			try {
+//				f.close();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+		}
 		
 	}
 	
-	void div(int a,int b) throws IOException {
+	public static void div(int a,int b) throws IOException {
 		if(b!=0) {
 			save(a/b);
 		}
 		save(0);
 	}
 	
-	void save(int d) throws IOException {
-		FileOutputStream f = new FileOutputStream("X:/file.txt");
+	public static void save(int d) throws IOException {
+		f = new FileOutputStream("X:/file.txt");
 	}
 
 }
