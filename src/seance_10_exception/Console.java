a59cc9b663b5e2519d120db2e728554f0a4db36b
Imzoughene Youssef <benfarice@gmail.com>
2020-01-05 10:36:16 +0100

    seance 10 throws key word

--- src/seance_10_exception/Console.java
+++ src/seance_10_exception/Console.java
@@ -1,22 +1,23 @@
 package seance_10_exception;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Console {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 	
-	void div(int a,int b) {
+	void div(int a,int b) throws IOException {
 		if(b!=0) {
 			save(a/b);
 		}
 		save(0);
 	}
 	
-	void save(int d) {
+	void save(int d) throws IOException {
 		FileOutputStream f = new FileOutputStream("X:/file.txt");
 	}
 
